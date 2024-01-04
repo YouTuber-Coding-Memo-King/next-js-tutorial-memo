79f2a57bac30314e911eb88221aca3dd67db6632
YouTuber Coding-Memo-King <148295116+YouTuber-Coding-Memo-King@users.noreply.github.com>
2024-01-04 19:37:34 +0900

    Need to disable some ESLint rules

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
 
-          <img
+          <Image
             src="/hero-desktop.png"
             width={1000}
             height={760}
@@ -53,7 +53,7 @@ export default function Page() {
             alt="Screenshots of the dashboard project showing desktop version"
           />
 
-          <img
+          <Image
             src="/hero-mobile.png"
             width={560}
             height={620}

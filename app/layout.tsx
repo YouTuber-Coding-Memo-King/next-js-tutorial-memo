0ae8658e91bc4b77ae010b897426be59bcac3e72
YouTuber Coding-Memo-King <148295116+YouTuber-Coding-Memo-King@users.noreply.github.com>
2024-03-17 14:02:24 +0900

    correct url

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,18 +31,17 @@ export default function RootLayout({
 
       <meta property="og:url" content="https://dptest-git-chapter16-youtuber-coding-memo-kings-projects.vercel.app?_vercel_share=ga66n4kk9stu8agivl2lukxahdrmnai8"></meta>
       <meta property="og:type" content="website"></meta>
-      <meta property="og:title" content=""></meta>
-      <meta property="og:description" content=""></meta>
-      <meta property="og:image" content=""></meta>
+      <meta property="og:title" content="Tutorial Chapter 16"></meta>
+      <meta property="og:description" content="turorial"></meta>
+      <meta property="og:image" content="opengraph-image.png"></meta>
 
 
       <meta name="twitter:card" content="summary_large_image"></meta>
       <meta property="twitter:domain" content="dptest-git-chapter16-youtuber-coding-memo-kings-projects.vercel.app"></meta>
       <meta property="twitter:url" content="https://dptest-git-chapter16-youtuber-coding-memo-kings-projects.vercel.app?_vercel_share=ga66n4kk9stu8agivl2lukxahdrmnai8"></meta>
       <meta name="twitter:title" content=""></meta>
-      <meta name="twitter:description" content=""></meta>
-      <meta name="twitter:image" content=""></meta>
-      <meta name="twitter:image" content="https://yt3.googleusercontent.com/yDKBqDHpwXYDGMFQYoLgZSfJ9MmtaayTJ8gB1cJjzwDIlf1HU4XNJxPIhUJ9fsmk13BMCfSzzOw=s900-c-k-c0x00ffffff-no-rj"></meta>
+      <meta name="twitter:description" content="tutorial"></meta>
+      <meta name="twitter:image" content="opengraph-image.png"></meta>
 
 
       <link rel="icon" href="react.svg" />

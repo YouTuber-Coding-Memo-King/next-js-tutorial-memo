efa2bfcccb976f3fa230d9d5cec895757cf64094
YouTuber Coding-Memo-King <148295116+YouTuber-Coding-Memo-King@users.noreply.github.com>
2024-03-17 15:15:11 +0900

    chapter 16

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,26 +23,12 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      {/* <meta property="og:title" content="Title Here" />
-      <meta property="og:description" content="Description Here" />
-      <meta property="og:image" content="opengraph-image.png" />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="600" /> */}
 
       <meta property="og:url" content="https://dptest-git-chapter16-youtuber-coding-memo-kings-projects.vercel.app?_vercel_share=ga66n4kk9stu8agivl2lukxahdrmnai8"></meta>
       <meta property="og:type" content="website"></meta>
-      <meta property="og:title" content="Tutorial Chapter 16"></meta>
-      <meta property="og:description" content="turorial"></meta>
+      <meta property="og:title" content="Next.js Chapter 16. 메타 데이터"></meta>
+      <meta property="og:description" content="Next.js하시느라 고생하셨습니다. 이제 구독!"></meta>
       <meta property="og:image" content="https://github.com/YouTuber-Coding-Memo-King/dptest/blob/chapter16/public/coding-memo-king.png?raw=true"></meta>
-
-
-      <meta name="twitter:card" content="summary_large_image"></meta>
-      <meta property="twitter:domain" content="dptest-git-chapter16-youtuber-coding-memo-kings-projects.vercel.app"></meta>
-      <meta property="twitter:url" content="https://dptest-git-chapter16-youtuber-coding-memo-kings-projects.vercel.app?_vercel_share=ga66n4kk9stu8agivl2lukxahdrmnai8"></meta>
-      <meta name="twitter:title" content=""></meta>
-      <meta name="twitter:description" content="tutorial"></meta>
-      <meta name="twitter:image" content="https://github.com/YouTuber-Coding-Memo-King/dptest/blob/chapter16/public/coding-memo-king.png?raw=true"></meta>
-
 
       <link rel="icon" href="react.svg" />
       <body className={`${inter.className} antialiased`}>{children}</body>
